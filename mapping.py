0d00223698784aa074f04411b8f51f1ca5b1cfa6
Sanchaksh Kaul <43877199+Sanchaksh@users.noreply.github.com>
2020-07-20 23:07:08 +0530

    Add files via upload

--- mapping.py
+++ mapping.py
@@ -6,12 +6,23 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_p(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation <= 3000:
+        return 'orange'
+    else:
+        return 'red'
 
-map = folium.Map(location = [38.58, -99.09], zoom_start = 6, tiles = "OpenStreetMap")
+
+map = folium.Map(location = [38.58, -99.09], zoom_start = 6, tiles = "Mapbox Bright")
+
 fg = folium.FeatureGroup(name="Volcano Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location = [lt, ln], popup=folium.Popup( "The Height of the volcano is " + str(el) + " m", parse_html=True), icon=folium.Icon(color='blue')))
+    fg.add_child(folium.CircleMarker(location = [lt, ln], radius = 6, popup = str(el) + " m", fill_color = color_p(el), color = 'grey', fill_opacity = 0.7 ))
+
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig'), style_function = lambda x: {'fillColor' : 'yellow'}))
 
 map.add_child(fg)
 map.save("Map1.html")
